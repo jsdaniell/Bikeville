5f94d96aae95fc451940ce10f6e6e9b873ec01d5
José Daniel <jose.daniell@outlook.com>
2024-03-01 16:09:10 -0300

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,133 @@
+"use client"
 import Image from "next/image";
+import {useState} from "react";
+import bikeImage from "../assets/bike.png"
 
 export default function Home() {
+  const [selectedTypeBike, setSelectedTypeBike] = useState(1)
+
+  const [isBycicleSelected, setIsBycicleSelected] = useState(true)
+
+  function handleSelectedTypeBike(typeNumber) {
+    setSelectedTypeBike(typeNumber)
+  }
+
+  function handleSelectedBike(botaoDeCima) {
+    setIsBycicleSelected(botaoDeCima === selectedTypeBike ? selectedTypeBike : !selectedTypeBike)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-wrap-reverse">
+      <section className="flex-[2] text-white bg-blue-900 min-h-full p-10 flex flex-col gap-28">
+        <h1 className="text-[250px] absolute top-56 left-40 font-black opacity-5">
+          I F
+        </h1>
+        <div>
+          Bikevile
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex gap-2">
+          <div className="flex-1 text-2xl ">If Mode</div>
+          <div className="flex flex-1 justify-end gap-2">
+            <div>
+              Overview
+            </div>
+            <div>
+              Specs
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-5 flex-col">
+          <div onClick={() => handleSelectedBike(true)} className={`${isBycicleSelected ? "" : "opacity-30"} cursor-pointer border-2 w-20 h-20 border-l-4 flex justify-center items-center`} style={{
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            borderColor: "rgba(255, 255, 255, 0.1)",
+            borderLeftColor: "red"
+          }}>
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+          <div onClick={() => handleSelectedBike(false)} className={`${isBycicleSelected ? "opacity-30" : ""} cursor-pointer border-2 w-20 h-20 border-l-4 flex justify-center items-center`} style={{
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            borderColor: "rgba(255, 255, 255, 0.1)",
+            borderLeftColor: "red"
+          }}>
+            <i className="fas fa-dharmachakra text-2xl"></i>
+          </div>
+        </div>
+        <div className="flex flex-grow items-end">
+          <div className="flex-1 px-10">
+            <div className="text-sm text-gray-400">
+              Price
+            </div>
+            <div className="text-xl">
+              R$ 2850
+            </div>
+          </div>
+          <button className="flex-1 bg-red-500 rounded-xl h-16">
+            + Add to cart
+          </button>
+        </div>
+      </section>
+      <section className="hidden md:flex flex-1 bg-white items-center">
+        <Image src={bikeImage} alt={"bike"} className="absolute left-80 rotate-12"/>
+      </section>
+      <section className="flex-[2] flex flex-col bg-white min-h-full p-10 gap-10">
+        <nav className="self-end text-blue-700">
+          <ul className="flex gap-10">
+            <li>
+              <a href="/about">Bikes</a>
+            </li>
+            <li>
+              <a href="/contact">Delivery</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <i className="fas fa-shopping-bag text-xl"></i>
+            </li>
+            <li>
+              <i className="fas fa-bars text-xl"></i>
+            </li>
+          </ul>
+        </nav>
+        <button
+          className="border-gray-200 w-36 border-2 rounded-md py-4 px-8 h-16 text-black flex justify-center items-center">
+          ← Back
+        </button>
+        <h1 className="text-4xl font-bold">
+          Configure the bike
+        </h1>
+        <div className="flex gap-4">
+          <div onClick={() => handleSelectedTypeBike(1)} className={`${selectedTypeBike === 1 ? "bg-blue-500 text-white shadow-2xl" : ""} flex-1 cursor-pointer rounded-xl border-gray-300 border h-24 justify-center items-center flex`}>
+            <i className="fab fa-stumbleupon-circle text-2xl"></i>
+          </div>
+          <div onClick={() => handleSelectedTypeBike(2)} className={`${selectedTypeBike === 2 ? "bg-blue-500 text-white shadow-2xl" : ""} flex-1 cursor-pointer rounded-xl border-gray-300 border h-24 justify-center items-center flex`}>
+            <i className="fab fa-searchengin text-2xl"></i>
+          </div>
+          <div onClick={() => handleSelectedTypeBike(3)} className={`${selectedTypeBike === 3 ? "bg-blue-500 text-white shadow-2xl" : ""} flex-1 cursor-pointer rounded-xl border-gray-300 border h-24 justify-center items-center flex`}>
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+          <div onClick={() => handleSelectedTypeBike(4)} className={`${selectedTypeBike === 4 ? "bg-blue-500 text-white shadow-2xl" : ""} flex-1 cursor-pointer rounded-xl border-gray-300 border h-24 justify-center items-center flex`}>
+            <i className="fas fa-tachometer-alt text-2xl"></i>
+          </div>
+        </div>
+        <h1 className="text-2xl">
+          Wheels
+        </h1>
+        <div className="flex gap-4">
+          <div className="flex-1 rounded-xl border-gray-300 border h-40 justify-center items-center flex">
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+          <div className="flex-1 rounded-xl border-gray-300 border h-40 justify-center items-center flex">
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+          <div className="flex-1 rounded-xl border-gray-300 border h-40 justify-center items-center flex">
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+          <div className="flex-1 rounded-xl border-gray-300 border h-40 justify-center items-center flex">
+            <i className="fas fa-bicycle text-2xl"></i>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
